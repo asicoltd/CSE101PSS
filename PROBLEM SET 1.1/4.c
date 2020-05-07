def9e30eb711435156c7c5e54314ecbccda2d38b
asicoltd <61615468+asicoltd@users.noreply.github.com>
2020-05-07 15:44:47 +0600

    Update 4.c

--- PROBLEM SET 1.1/4.c
+++ PROBLEM SET 1.1/4.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 
 int main()
-{   int x,n=0,y,i;
+{   int x,n=0;
     printf("Enter a Decimal number:");
     scanf("%d",&x);
-    i=x;
-    do{
-        y=x/2;
-        n=x%2+n*10;
-        if(x/2!=0){
-            x=y;}
+    int y = 0;
+    while( x > 0 ){
+        y = x % 2 + y*10;
+        x /= 2;
     }
-    while(y>0);
-
-    x=n+y;
-    n=0;
-    //Next part to xyz to zyx
-    do{
-    n=x%10+n*10;
-    y=x/10;
-    x=y;}
-    while(y>0);
-    n=n+y;
-    n=n*10;
-    printf("BINARY form of %d is %d",i,n);
-
+    while( y > 0 ){
+        x = y % 10 + x*10;
+        y /= 10;
+    }
+    printf("Binary Value :%d\n",x);
     return 0;
 }
