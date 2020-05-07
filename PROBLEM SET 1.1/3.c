931f80ac151f14625a90a01d6c18ede6e99c672b
asicoltd <61615468+asicoltd@users.noreply.github.com>
2020-05-07 15:44:07 +0600

    Update 3.c

--- PROBLEM SET 1.1/3.c
+++ PROBLEM SET 1.1/3.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 
 int main()
-{   int x,n=0,y,i;
+{   int x,n=0;
     printf("Enter a Decimal number:");
     scanf("%d",&x);
-    i=x;
-    do{
-        y=x/8;
-        n=x%8+n*10;
-        x=y;
+    int y = 0;
+    while( x > 0 ){
+        y = x % 8 + y*10;
+        x /= 8;
     }
-    while(y>0);
-    x=n+y;
-    n=0;
-    //Next part to xyz to zyx
-    do{
-    n=x%10+n*10;
-    y=x/10;
-    x=y;}
-    while(y>0);
-    n=n+y;
-    printf("Octal form of %d is %d",i,n);
-
+    while( y > 0 ){
+        x = y % 10 + x*10;
+        y /= 10;
+    }
+    printf("Octal Value :%d\n",x);
     return 0;
 }
